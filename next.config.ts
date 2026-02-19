557d2d013ee5e30036f5208c784a239d0452e3c1
Tassio Neves <tassions@hotmail.com>
2026-02-19 14:16:37 -0300

    chore: add prisma setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {},
+  serverExternalPackages: ["@prisma/client", "prisma"],
+  outputFileTracingIncludes: {
+    "/api/**/*": ["../src/generated/prisma/**/*"],
+  },
 };
 
 export default nextConfig;
