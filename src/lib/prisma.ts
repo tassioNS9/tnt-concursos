ad3097239ea12f3ed2e97957c2a26074c2a0bdc4
Tassio Neves <tassions@hotmail.com>
2026-02-27 13:24:59 -0300

    fix: change local path prisma generate

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaClient } from "../../src/app/generated/prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
