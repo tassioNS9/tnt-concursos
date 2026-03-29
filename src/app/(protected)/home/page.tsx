6b93149c33200d36bcfe56ec5bed034e94ae2670
Tassio Neves <tassions@hotmail.com>
2026-03-29 16:40:06 -0300

    feat: update initial page

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -1,51 +1,22 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Search, ArrowLeft } from "lucide-react";
-import { disciplinas, ano } from "@/app/constants/optionsSelectors";
-import { MultiSelect } from "../components/multiselect";
-export default function Home() {
-  return (
-    <div className="mx-auto flex w-full items-center p-8">
-      <div className="flex w-full flex-col px-16">
-        <h2 className="flex items-center gap-2 text-2xl font-bold">
-          {" "}
-          <ArrowLeft color="orange" />
-          Questões de Concursos
-        </h2>
-        <div className="mt-8 flex w-full flex-col gap-4 rounded bg-[#e5e1e1] px-2 py-3">
-          <div className="relative flex items-center">
-            <Search className="absolute left-2" color="#878282" size="16" />
-            <Input
-              type="text"
-              className="border-border bg-white px-7"
-              placeholder="Enuciado ou código da questão"
-            />
-          </div>
+"use client";
+import { FiltersQuestions } from "./components/filters-questions";
+import { useGetQuestions } from "@/app/actions/useQuetions";
+import QuestionItem from "./components/question-item";
 
-          <div className="grid grid-cols-3 gap-3">
-            <MultiSelect label="Disciplinas" options={disciplinas} />
-            <MultiSelect label="Ano" options={ano} />
-            <MultiSelect label="Disciplinas" options={disciplinas} />
-            <MultiSelect label="Disciplinas" options={disciplinas} />
-            <MultiSelect label="Disciplinas" options={disciplinas} />
+export default function Home() {
+  const { data: questions } = useGetQuestions();
+  return (
+    <div className="mx-auto flex w-full flex-col items-center p-8 lg:px-20">
+      <FiltersQuestions />
+      <div className="mt-6 flex w-full flex-col items-center gap-4 md:px-16">
+        {questions?.map((question) => (
+          <div
+            className="text-emerald-400 md:min-w-3xl lg:min-w-5xl"
+            key={question.id}
+          >
+            <QuestionItem question={question} />
           </div>
-          <h3 className="text-xl">
-            Filtros selecionados:{" "}
-            <span className="text-muted-foreground text-sm">
-              {" "}
-              os filtros aparecerão aqui
-            </span>
-          </h3>
-          <div className="flex justify-end">
-            <Button className="pointer" variant="link">
-              Limpar
-            </Button>
-            <Button className="pointer" variant="default">
-              Filtrar Filtros
-            </Button>
-          </div>
-        </div>
-        <h3 className="mt-3 w-full px-2">299.411 questões encontradas</h3>
+        ))}
       </div>
     </div>
   );
