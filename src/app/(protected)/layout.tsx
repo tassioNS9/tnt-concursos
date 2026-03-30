501c690c8f48908353f0912f7262dc862834087e
Tassio Neves <tassions@hotmail.com>
2026-03-30 16:06:15 -0300

    feat: add initial dropdown avatar

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,15 +1,50 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 import { AppSidebar } from "./components/app-sidebar";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="w-full">
-        <SidebarTrigger />
+      <SidebarInset>
+        <div className="flex items-center justify-between gap-2 p-4 text-shadow-mauve-700">
+          {/* Botão hambúrguer para mobile */}
+          <SidebarTrigger className="lg:hidden" />
+
+          <div className="flex items-center justify-center gap-2">
+            <Avatar className="h-10 w-10 rounded-full bg-gray-300 text-sm font-medium text-gray-600">
+              <DropdownMenu>
+                <DropdownMenuTrigger className="w-full">
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="shadcn"
+                  />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Settings</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </Avatar>
+            <span className="text-1xl text-dark-var-3-color ml-2 font-semibold">
+              Olá, Usuário!
+            </span>
+          </div>
+        </div>
+
         {children}
-      </main>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
