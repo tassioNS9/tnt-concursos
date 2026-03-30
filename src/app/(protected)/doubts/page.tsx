9eca3df9a868a1bc8893bca688c9fcec1023ad80
Tassio Neves <tassions@hotmail.com>
2026-03-30 16:03:49 -0300

    feat: add initial doubts  page

---
+++ src/app/(protected)/doubts/page.tsx
@@ -0,0 +1,15 @@
+export const DoubtsPage = () => {
+  return (
+    <div className="mx-auto flex w-full flex-col items-center p-8 lg:px-20">
+      <h1 className="text-2xl font-bold tracking-tight text-gray-900">
+        Em breve, a página de dúvidas estará disponível!
+      </h1>
+      <p className="mt-2 text-lg text-gray-600">
+        Estamos trabalhando para trazer essa funcionalidade em breve. Fique
+        atento às atualizações!
+      </p>
+    </div>
+  );
+};
+
+export default DoubtsPage;
