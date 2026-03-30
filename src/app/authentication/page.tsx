f2d13e5c18abd792dbb65a054e6db88b18926e69
Tassio Neves <tassions@hotmail.com>
2026-03-30 16:06:39 -0300

    feat: add initial authentication page

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -6,16 +6,17 @@ const AuthenticationPage = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <div className="flex items-center justify-center">
-        <div className="flex h-[382px] w-100 flex-col items-center justify-center gap-5 bg-blue-950 p-6">
-          <h2 className="text-2xl font-bold text-white">
+        <div className="bg-dark-blue-1-color flex h-[382px] w-100 flex-col items-center justify-center gap-5 rounded-l-4xl p-6">
+          <h2 className="text-foreground-orange text-3xl text-[1.875rem] leading-tight font-bold">
             Bem vindo à nossa plataforma
           </h2>
-          <h3 className="text-3xl font-semibold text-white">
-            TNT Concursos representa, na atualidade, o que há de melhores
-            questões na sua preparação para os concursos no Brasil
+          <h3 className="text-1xl font-semibold text-white">
+            <span className="text-foreground-orange">TNT Concursos </span>
+            representa, na atualidade, o que há de melhores questões na sua
+            preparação para os concursos no Brasil
           </h3>
         </div>
-        <div>
+        <div className="bg-foreground-orange rounded-r-4xl">
           <Tabs defaultValue="login" className="w-[400px] p-6">
             <TabsList className="grid w-full grid-cols-2">
               <TabsTrigger value="login">Login</TabsTrigger>
