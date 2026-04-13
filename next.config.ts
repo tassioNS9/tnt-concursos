acefbe096d9ad55a44691685e6234a3e97794a05
Tassio Neves <tassions@hotmail.com>
2026-04-13 15:49:05 -0300

    chore: configure image domains for external sources

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {},
+  images: {
+    domains: [
+      "aws-1-us-east-1.pooler.supabase.com",
+      "lh3.googleusercontent.com",
+      "avatars.githubusercontent.com",
+    ],
+  },
   serverExternalPackages: ["@prisma/client", "prisma"],
   outputFileTracingIncludes: {
     "/api/**/*": ["../src/app/generated/prisma/**/*"],
