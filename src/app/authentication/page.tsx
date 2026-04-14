5ff9fddec1a640b33e87ce57f5bb0c12f2f7a754
Tassio Neves <tassions@hotmail.com>
2026-04-14 16:09:47 -0300

    feat: add session validation and redirect to home on authentication page

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -5,8 +5,18 @@ import { BookOpen } from "lucide-react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import LoginForm from "./components/login-form";
 import SignUpForm from "./components/sign-up-form";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 const AuthenticationPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session?.user) {
+    redirect("/home");
+  }
+
   return (
     <main className="flex min-h-screen">
       <section className="bg-primary relative hidden w-1/2 overflow-hidden lg:flex">
