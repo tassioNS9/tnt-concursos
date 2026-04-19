473f7d06085a831ee17b5ee66dd2048016655e9f
Tassio Neves <tassions@hotmail.com>
2026-04-19 17:12:54 -0300

    feat: add filter options for disciplines, years, jurys, organs, and positions

---
+++ src/optionsSelectors.ts
@@ -0,0 +1,48 @@
+export type FilterItem = string[];
+
+export const disciplines: FilterItem = [
+  "Português",
+  "Matemática",
+  "Informática",
+  "Direito Constitucional",
+  "Raciocínio Lógico",
+  "Direito Administrativo",
+  "Direito Penal",
+];
+
+export const years: FilterItem = [
+  "2017",
+  "2018",
+  "2019",
+  "2020",
+  "2021",
+  "2022",
+  "2023",
+  "2024",
+  "2025",
+];
+
+export const jurys: FilterItem = [
+  "CESPE",
+  "FCC",
+  "FGV",
+  "VUNESP",
+  "IBFC",
+  "IBAM",
+];
+
+export const organs: FilterItem = [
+  "Prefeitura Municipal de Salvador",
+  "Prefeitura Municipal de Feira de Santana",
+  "Prefeitura Municipal de Camaçari",
+  "Prefeitura Municipal de Lauro de Freitas",
+  "Prefeitura Municipal de Ilhéus",
+];
+
+export const positions: FilterItem = [
+  "Assistente Administrativo",
+  "Auxiliar Administrativo",
+  "Técnico de TI",
+  "Procurador",
+  "Assistente",
+];
